b47e8305f29da21865ba44fcbd9d4e6a14735129
unknown <wensyaustin@foxmail.com>
2022-07-24 15:43:58 -0400

    update may

--- main.py
+++ main.py
@@ -12,16 +12,45 @@ from PyQt5.QtWidgets import (
     QApplication, QDialog, QMainWindow, QMessageBox, QWidget, QFileDialog, QRubberBand
 )
 from PyQt5.uic import loadUi
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtGui import QIcon, QPixmap, QImage  
 from PyQt5 import QtCore,QtGui
 
-from main_ui import Ui_BitRecognition
+from main_ui_0424 import Ui_BitRecognition
 
 # Only needed for access to command line arguments
 import sys
 from PIL import ImageQt
 import torch
 import numpy as np
+
+# Library and function for image recognition
+import torch
+from models.common import DetectMultiBackend
+from utils.general import check_img_size, non_max_suppression
+from utils.torch_utils import select_device, time_sync
+from pathlib import Path
+import cv2
+from PIL import Image
+import numpy as np
+from utils.augmentations import letterbox
+from PIL import ImageQt
+def hex2rgb(h):  # rgb order (PIL)
+    return tuple(int(h[1 + i:1 + i + 2], 16) for i in (0, 2, 4))
+hex = ('FF3838', 'FF9D97', 'FF701F', 'FFB21D', 'CFD231', '48F90A', '92CC17', '3DDB86', '1A9334', '00D4BB',
+       '2C99A8', '00C2FF', '344593', '6473FF', '0018EC', '8438FF', '520085', 'CB38FF', 'FF95C8', 'FF37C7')
+
+def qtpixmap_to_cvimg(qtpixmap):
+
+    qimg = qtpixmap.toImage()
+    temp_shape = (qimg.height(), qimg.bytesPerLine() * 8 // qimg.depth())
+    temp_shape += (4,)
+    ptr = qimg.bits()
+    ptr.setsize(qimg.byteCount())
+    result = np.array(ptr, dtype=np.uint8).reshape(temp_shape)
+    result = result[..., :3]
+
+    return result
+
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
@@ -39,13 +68,14 @@ class Window(QMainWindow, Ui_BitRecognition):
         self.rubberBand = QRubberBand(QRubberBand.Rectangle, self)
         self.origin = QtCore.QPoint()
         self.cropQPixmap = None
-        PATH = "./model.pth"
+        PATH = "./model/model_final.pth"
         self.model = torch.load(PATH)
         print(self.model)
     #%%
         #buttons and events
         self.LoadImage_button.clicked.connect(self.openFileNameDialog)
         self.Inference_button.clicked.connect(self.inference)
+        self.Detection_button.clicked.connect(self.inference_recognition)
         #self.connectSignalsSlots()
     #%%    
     def mousePressEvent(self, event):
@@ -69,7 +99,7 @@ class Window(QMainWindow, Ui_BitRecognition):
            
            #calculate image relative position:
            img_label_coordinates = [self.ImgLabel.geometry().x(),self.ImgLabel.geometry().y(),self.ImgLabel.geometry().width(),self.ImgLabel.geometry().height()]
-           print(img_label_coordinates)
+           #print(img_label_coordinates)
            #image_origin_pos = [20+int((820-self.img_rect.width())/2),30+int((600-self.img_rect.height())/2)]
            image_origin_pos = [img_label_coordinates[0]+int((img_label_coordinates[2]-self.img_rect.width())/2),img_label_coordinates[1]+int((img_label_coordinates[3]-self.img_rect.height())/2)]
            true_rect = QtCore.QRect(currentQRect.x()-image_origin_pos[0],currentQRect.y()-image_origin_pos[1],currentQRect.width(),currentQRect.height())
@@ -98,8 +128,8 @@ class Window(QMainWindow, Ui_BitRecognition):
         fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Image Files (*.jpg,*.png,*jpeg)", options=options)
         if fileName:
             self.img_path = fileName
-            self.pixmap = QPixmap(fileName)
-            self.pixmap = self.pixmap.scaled(self.ImgLabel.width(), self.ImgLabel.height(), QtCore.Qt.KeepAspectRatio,QtCore.Qt.FastTransformation)
+            self.pixmap_original = QPixmap(fileName)
+            self.pixmap = self.pixmap_original.scaled(self.ImgLabel.width(), self.ImgLabel.height(), QtCore.Qt.KeepAspectRatio,QtCore.Qt.FastTransformation)
             #pixmap = pixmap.scaled(self.ImgLabel.width(), self.ImgLabel.height())
             self.img_rect = self.pixmap.rect()
             self.ImgLabel.setPixmap(self.pixmap)
@@ -108,7 +138,7 @@ class Window(QMainWindow, Ui_BitRecognition):
             #print(fileName)
     #%%        
     def inference(self):
-        print(self.cropQPixmap)
+        #print(self.cropQPixmap)
         if self.cropQPixmap:
             PIL_image = ImageQt.fromqpixmap(self.cropQPixmap)
             PIL_image = PIL_image.resize((128,128))
@@ -116,10 +146,106 @@ class Window(QMainWindow, Ui_BitRecognition):
             self.ImgNameTextEdit.setPlainText("no ROI selected")  
             return
         input_np_array = np.rollaxis(np.array(PIL_image),2,0)[None,...]
-        print(input_np_array.shape)
-        prediction = self.model(torch.from_numpy(input_np_array).float())
-        self.probability_TextEdit.setPlainText("Probability: {}".format(prediction.item())) 
+        #print(input_np_array.shape)
+        output = self.model(torch.from_numpy(input_np_array).float())
+        _, prediction = output.max(1)
+        self.probability_TextEdit.setPlainText("Current mode: image classification\n\nObject Information:\n   Probability of being bits: {}".format(prediction.item())) 
+    #%%
+    # image recognition inference code 
+    ###########################################################################
+    def inference_recognition(self): 
+        self.probability_TextEdit.setPlainText("Current mode: image recognition\n\nObject Information:\n   Probability of being bits: {}".format(1)) 
+        color = hex2rgb('#' + hex[6])
+        
+        # Model Parameters
+        conf_thres=0.25  # confidence threshold
+        iou_thres=0.45
+        max_det=1000
+        classes=None  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms=False
+    
+        # Visualization Parameters
+        line_thickness=3
+    
+    
+        FILE = Path(__file__).resolve()
+        ROOT = FILE.parents[0]  # YOLOv5 root directory
+        # candidate model: yolov5n, yolov5s, yolov5m, yolov5l, yolov5x
+        weights = ROOT / 'weights/best.pt'  
+        data=ROOT / 'data/coco128.yaml'
+        device = ''
+        device = select_device(device)
+        #model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
+        model = DetectMultiBackend(weights, device=device, dnn=False, data=data, fp16=False)
+    
+    
+        stride, names, pt = model.stride, model.names, model.pt
+        imgsz=(640, 640)
+        imgsz = check_img_size(imgsz, s=stride)  # check image size
+        model.warmup(imgsz=(1 if pt else 1, 3, *imgsz))  # warmup
+        
+        # load data
+        dt = np.array([0.0, 0.0, 0.0, 0.0])
+        t1 = time_sync()
+        img_show = qtpixmap_to_cvimg(self.pixmap_original)  
+        # refer to ./utils/augmentations.py
+        img = letterbox(img_show, imgsz, stride=stride, auto=pt)[0]
+        img = img.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+        img = np.ascontiguousarray(img)
+    
+        # make a copy for annotation
+        img_raw  = img.copy()
+    
+        img = img[None,...]
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if model.fp16 else img.float()  # uint8 to fp16/32
+        img /= 255  # 0 - 255 to 0.0 - 1.0
+        if len(img.shape) == 3:
+            img = img[None]  # expand for batch dim
+        t2 = time_sync()
+        dt[0] = t2 - t1
+        # inference
+        pred = model(img, augment=False, visualize=False)
+        t3 = time_sync()
+        dt[1] = t3 - t2
+        # NMS- non max suppresion
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+        #print(pred[0].shape)
+        t4 = time_sync()
+        dt[2] = t4 - t3
+        # Process predictions
+        # pred after NMS consists of [n,6] elements, whereas n = nums of detected samples
+        # 6 consists of [[x1,y1,x2,y2], conf, label] whereas conf = probability of pred, label = class num
+    
+        # 1. recover actual x1,y1,x2,y2 with respect to original pictures
+        x,y,x2,y2 = 0,0,0,0
+        img_drawn = img_show.copy() 
+        for i in range(pred[0].shape[0]):
+            x,y,x2,y2 = pred[0].data[i][0].item(), pred[0].data[i][1].item(), pred[0].data[i][2].item(),pred[0].data[i][3].item()
+            x,y = int(x*img_show.shape[1]/img_raw.shape[2]), int(y*img_show.shape[0]/img_raw.shape[1]) 
+            x2,y2 = int(x2*img_show.shape[1]/img_raw.shape[2]), int(y2*img_show.shape[0]/img_raw.shape[1])
             
+            # draw rectangle of object
+            img_drawn = cv2.rectangle(img_drawn,(x,y),(x2,y2),color,line_thickness) #(0,255,0)
+            # draw txt
+            txt = "{:.3f}".format(pred[0].data[i][4].item())
+            # draw background of txt 
+            (w, h), _ = cv2.getTextSize(txt, cv2.FONT_HERSHEY_SIMPLEX, 0.6, line_thickness)
+            img_drawn = cv2.rectangle(img_drawn, (x, y - 20), (x + w, y), color, -1)
+            img_drawn = cv2.putText(img_drawn, txt, (x, y - 5),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,255), 2, lineType=cv2.LINE_AA)
+    
+        
+        img_final = cv2.cvtColor(img_drawn, cv2.COLOR_BGR2RGB)
+        #im_pil = Image.fromarray(img_final)  
+        
+        height, width, channel = img_final.shape
+        bytesPerLine = 3 * width
+        qImg = QImage(img_final.data, width, height, bytesPerLine, QImage.Format_RGB888)
+
+        im_qt= qImg.scaled(self.ImgLabel.width(), self.ImgLabel.height(), QtCore.Qt.KeepAspectRatio,QtCore.Qt.FastTransformation)
+        self.ImgLabel.setPixmap(QPixmap(im_qt))
+    
 #%%
 # Create a Qt widget, which will be our window.
 #window = QWidget()
--- train.py
+++ train.py
@@ -13,116 +13,47 @@ import torch.nn.functional as F
 from torchvision import transforms, utils, datasets
 from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler
 import torchvision.models as models
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 
 import os
 #import pandas as pd
 from torchvision.io import read_image
 import numpy as np
 
-'''
-class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file)
-        self.img_dir = img_dir
-        self.transform = transform
-        self.target_transform = target_transform
 
-    def __len__(self):
-        return len(self.img_labels)
+EPOCH = 150
+best_acc = 0
 
-    def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 1]
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-        return image, label
-'''
-
-
-learning_rate = 0.0005
-#image_transforms = {"train": transforms.Compose([transforms.Resize((224, 224)),transforms.ToTensor()]),
-#                    "test": transforms.Compose([transforms.Resize((224, 224)),transforms.ToTensor()])}
-
-image_transforms = {"train": transforms.Compose([transforms.Resize((128, 128)),transforms.ToTensor()]),
+learning_rate = 0.0001 # was 0.0001
+#transforms.RandomRotation(10),
+image_transforms = {"train": transforms.Compose([transforms.Resize((128, 128)),
+                                                 transforms.RandomHorizontalFlip(0.1),
+                                                 transforms.RandomVerticalFlip(p=0.1),
+                                                 transforms.ToTensor()]),
                     "test": transforms.Compose([transforms.Resize((128, 128)),transforms.ToTensor()])}
 
-train_data = datasets.ImageFolder("D:/playground/gui_bit_recog/data/train", transform=image_transforms["train"])
-test_data = datasets.ImageFolder("D:/playground/gui_bit_recog/data/test", transform=image_transforms["test"])
+train_data = datasets.ImageFolder("D:/pytorch-project/data/bit_recog_data/train", transform=image_transforms["train"])
+test_data = datasets.ImageFolder("D:/pytorch-project/data/bit_recog_data/validation", transform=image_transforms["test"])
 
 
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=1,shuffle=True)
 val_loader = torch.utils.data.DataLoader(test_data, batch_size=1)
 
 
-model = models.resnet18(pretrained=True)
+model = models.resnet50(pretrained=True)
 
-#print(model)
-model.fc = nn.Linear(512, 1)
-model = nn.Sequential(
-    model,
-    nn.Sigmoid()
-)
-print(model)
+# Finetune convertion
+num_ftrs = model.fc.in_features
+# Here the size of each output sample is set to 2.
+# Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
+model.fc = nn.Linear(num_ftrs, 2)
 
-class Net(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        x = nn.Sigmoid(x)
-        return x
-
-
-#model = Net()
-
-criterion = nn.BCEWithLogitsLoss()
-optimizer = torch.optim.Adam(model.parameters(),lr = learning_rate)
+#criterion = nn.BCEWithLogitsLoss()
+criterion = nn.CrossEntropyLoss()
+#optimizer = torch.optim.Adam(model.parameters(),lr = learning_rate)
+optimizer = optim.SGD(model.parameters(), lr=learning_rate,
+                     momentum=0.9, weight_decay=5e-4)
 train_losses = []
-'''
-for epoch in range(1, num_epochs=15):
-    train_loss = 0.0
-    model.train()
-    for data, target in train_loader:
-  
-        optimizer.zero_grad()
-        #forward-pass
-        output = model(data)
-        loss = criterion(output, target)
-        #backward-pass
-        loss.backward()
-        # Update the parameters
-        optimizer.step()
-        # Update the Training loss
-        train_loss += loss.item() * data.size(0)
-'''
-def acc(y_pred, y_test):
-    acc = 1-abs(y_pred-y_test)/y_test
-    return acc
-#def binary_acc(y_pred, y_test):
-#    #y_pred_tag = torch.log_softmax(y_pred, dim = 0)
-#    #_, y_pred_tags = torch.max(y_pred_tag, dim = 0)
-#    y_pred_tag = torch.log_softmax(y_pred)
-#    _, y_pred_tags = torch.max(y_pred_tag)
-#    correct_results_sum = (y_pred_tags == y_test).sum().float()
-#    acc = correct_results_sum/y_test.shape[0]
-#    acc = torch.round(acc * 100)
-#    return acc
 
 accuracy_stats = {
     'train': [],
@@ -133,51 +64,58 @@ loss_stats = {
     "val": []
 }
 
-#for e in range(1, 15):
 
 # TRAINING
-train_epoch_loss = 0
-train_epoch_acc = 0
-model.train()
-for i in range(0,30):
-  for X_train_batch, y_train_batch in train_loader:
-    y_train_batch = y_train_batch
-    #X_train_batch, y_train_batch = X_train_batch.to(device), y_train_batch.to(device)
-    optimizer.zero_grad()
-    y_train_pred = model(X_train_batch).view(-1)
-    #print("prediction: {}".format(y_train_pred.size()))
-    #print(y_train_pred)
-    #print("ground truth: {}".format(y_train_batch.size()))
-    #print(y_train_batch)
-       
-    train_loss = criterion(y_train_pred, y_train_batch.float())
-    #train_loss = criterion(y_train_batch.float(), y_train_batch.float())
-    train_acc = acc(y_train_pred, y_train_batch)
-    print("pred: {}".format(y_train_pred))
-    train_loss.backward()
-    optimizer.step()
-    train_epoch_loss += train_loss.item()
-    train_epoch_acc += train_acc.item()
-    print("train loss: {}".format(train_loss))
-    print("train acc: {}\n".format(train_acc))
-'''
+def train(epoch):
+    model.train()
+    correct = 0
+    total = 0
+    train_epoch_loss = 0
+    loop = tqdm(train_loader,total = len(train_loader))
+    for batch_idx, (x_train_batch, y_train_batch) in enumerate(loop):
+        optimizer.zero_grad()
+        output = model(x_train_batch) 
+        train_loss = criterion(output, y_train_batch)
+        train_loss.backward()
+        optimizer.step()
+        
+        train_epoch_loss += train_loss.item()
+        _, predicted = output.max(1)
+        #print("\nprediction: {}  ground truth: {}".format(predicted.item(), y_train_batch.item()))
+        total += y_train_batch.size(0)
+        correct += predicted.eq(y_train_batch).sum().item()
+        acc = correct/total
+        
+        loop.set_description(f"Epoch [{epoch}/{EPOCH}]")
+        loop.set_postfix(loss=(train_epoch_loss/(batch_idx+1)), accuracy=acc)
+    
 # VALIDATION
-with torch.no_grad():
+def validation():
+    global best_acc
     model.eval()
     val_epoch_loss = 0
-    val_epoch_acc = 0
-    for X_val_batch, y_val_batch in val_loader:
-        #X_val_batch, y_val_batch = X_val_batch.to(device), y_val_batch.to(device)
-        y_val_pred = model(X_val_batch).squeeze()
-        y_val_pred = torch.unsqueeze(y_val_pred, 0)
-        val_loss = criterion(y_val_pred, y_val_batch.float())
-        val_acc = acc(y_val_pred, y_val_batch)
-        val_epoch_loss += val_loss.item()
-        val_epoch_acc += val_acc.item()
-        loss_stats['train'].append(train_epoch_loss/len(train_loader))
-        loss_stats['val'].append(val_epoch_loss/len(val_loader))
-        accuracy_stats['train'].append(train_epoch_acc/len(train_loader))
-        accuracy_stats['val'].append(val_epoch_acc/len(val_loader))
-        #print(f'Epoch {e+0:02}: | Train Loss: {train_epoch_loss/len(train_loader):.5f} | Val Loss: {val_epoch_loss/len(val_loader):.5f} | Train Acc: {train_epoch_acc/len(train_loader):.3f}| Val Acc: {val_epoch_acc/len(val_loader):.3f}')
-'''
-torch.save(model, "./model.pth")
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (x_val_batch, y_val_batch) in enumerate(val_loader):
+            output = model(x_val_batch) 
+            val_loss = criterion(output, y_val_batch)
+            val_epoch_loss += val_loss.item()
+            _, predicted = output.max(1)
+            total += y_val_batch.size(0)
+            correct += predicted.eq(y_val_batch).sum().item()
+    acc = correct/total
+    print("val acc: {}".format(acc))
+    print("best acc: {}".format(best_acc))
+    if acc > best_acc:
+        best_acc = acc
+        # add saving logic here
+        torch.save(model, "./model_best.pth")
+
+if __name__ == "__main__":
+    for epoch in range(0, EPOCH):
+        train(epoch)
+        # val and save every 5 epochs
+        if epoch%5 == 0:
+            validation()
+    torch.save(model, "./model_final.pth")
