f1ead664700c202c29cd521c6778ada349279cd7
Austin Wen <41589262+Sawyer117@users.noreply.github.com>
2024-01-24 11:44:30 -0500

    Update main_ui_0424.py

--- main_ui_0424.py
+++ main_ui_0424.py
@@ -5,7 +5,10 @@
 # Created by: PyQt5 UI code generator 5.9.2
 #
 # WARNING! All changes made in this file will be lost!
-
+# H.J. Eysenck
+#Die wissenschäftliche Erforschung der Astrologie und die Förderung nach ‘naïven’ Versuchspersonen
+#Zeitschrift für Parapsychologie und Grenzgebiete der Psychologie
+#(1981)
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_BitRecognition(object):
